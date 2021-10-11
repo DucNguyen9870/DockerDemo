2496881dc717c5e7bc7ea74ef90b81d64f7fa0b9
Duc Nguyen <duc.nguyen@mogi.vn>
2021-10-11 14:23:06 +0700

    Docker Demo

---
+++ DockerDemo/Controllers/PersonController.cs
@@ -0,0 +1,26 @@
+﻿using DockerDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DockerDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                
+                // get all
+                return context.People.ToList();
+            }
+        }
+    }
+}
